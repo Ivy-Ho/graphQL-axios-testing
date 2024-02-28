ff0aea1fc2abe44703a0772144b406db1482f517
ivy.ho <ivy.ho@udngroup.com>
2024-02-28 23:56:06 +0800

    update post url path

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const userRequest = (data) =>
-  axios.post("https://countries.trevorblades.com/", data);
+  axios.post("https://countries.trevorblades.com/graphql", data);
 
 export const apiGetContinents = (query) => userRequest(query);
 export const apiGetCountries = (postObj) => userRequest(postObj);
